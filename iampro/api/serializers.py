8daf499e00255953dc0ee2f4f5dbda2392a35447
robin kumar <robinkr.8cl@gmail.com>
2021-06-30 22:19:06 +0530

    apiv1.0 ends

--- iampro/api/serializers.py
+++
@@ -1,6 +0,0 @@
-from django.contrib.auth.models import User
-from rest_framework import fields, serializers
-
-class UserSerializer(serializers.ModelSerializer):
-    model= User;
-    fields=['username', 'password', 'email']
--- iampro/api/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('register', views.RegView.as_view()),
-    path('login', views.LoginView.as_view()),
-    path('logout', views.LogoutView.as_view()),
-]
